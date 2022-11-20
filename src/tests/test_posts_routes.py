47fe983812de9e4b74d440960fc92827260e8f96
Gabriel <gbrcenciati@gmail.com>
2022-11-20 20:44:42 -0300

    fix: tests name

--- src/tests/test_posts_routes.py
+++ src/tests/test_posts_routes.py
@@ -1,7 +1,6 @@
 from json import dumps
 
 from fastapi import status
-from fastapi.responses import Response
 from fastapi.testclient import TestClient
 
 from src.models.post import Post
@@ -18,20 +17,20 @@ post: Post = Post(
 
 
 # POST /api/v1/posts
-def test_if_status_code_is_equal_200_when_create_a_post() -> None:
-    response: Response = client.post(f"{url}", data=dumps(post.dict()))
+def test_if_status_code_is_equal_201_when_create_a_post() -> None:
+    response = client.post(f"{url}", data=dumps(post.dict()))
     assert response.status_code == status.HTTP_201_CREATED
 
 
 # GET /api/v1/posts
 def test_if_status_code_is_equal_200_when_get_all_posts() -> None:
-    response: Response = client.get(f"{url}")
+    response = client.get(f"{url}")
     assert response.status_code == status.HTTP_200_OK
 
 
 # GET /api/v1/posts/{id}
 def test_if_status_code_is_equal_200_when_get_one_post() -> None:
-    response: Response = client.get(f"{url}/{1}")
+    response = client.get(f"{url}/{1}")
     assert response.status_code == status.HTTP_200_OK
 
 
@@ -43,13 +42,11 @@ def test_if_status_code_is_equal_200_when_update_a_post() -> None:
         title="Edited title",
         content="Edited content",
     )
-    response: Response = client.put(
-        f"{url}/{1}", data=dumps(updated_post.dict())
-    )
+    response = client.put(f"{url}/{1}", data=dumps(updated_post.dict()))
     assert response.status_code == status.HTTP_200_OK
 
 
 # DELETE /api/v1/posts/{id}
-def test_if_status_code_is_equal_200_when_delete_a_post() -> None:
-    response: Response = client.delete(f"{url}/{1}")
+def test_if_status_code_is_equal_204_when_delete_a_post() -> None:
+    response = client.delete(f"{url}/{1}")
     assert response.status_code == status.HTTP_204_NO_CONTENT
