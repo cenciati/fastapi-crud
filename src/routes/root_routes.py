910de8cb60f24cc8c5049d493fa1d02ff9385e1d
Gabriel <gbrcenciati@gmail.com>
2022-11-21 11:07:51 -0300

    chore: modify response format

--- src/routes/root_routes.py
+++ src/routes/root_routes.py
@@ -1,15 +1,13 @@
 from fastapi import APIRouter, status
-from fastapi.responses import JSONResponse
 
 root_routes = APIRouter()
 
 
-@root_routes.get("/api/v1")
-async def root() -> JSONResponse:
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content={
-            "data": ["Welcome to my personal blog!"],
-            "links": {"self": "/", "posts": "/posts"},
-        },
-    )
+@root_routes.get(
+    "/api/v1", status_code=status.HTTP_200_OK, response_model=dict
+)
+async def root() -> dict:
+    return {
+        "data": ["Welcome to my personal blog!"],
+        "links": {"self": "/", "posts": "/posts"},
+    }
