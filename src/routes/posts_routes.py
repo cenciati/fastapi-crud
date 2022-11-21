2d413f37e6e22eb4f8c8a709de4bee21b55654bf
Gabriel <gbrcenciati@gmail.com>
2022-11-21 10:59:32 -0300

    add: implemented ORM

--- src/routes/posts_routes.py
+++ src/routes/posts_routes.py
@@ -1,133 +1,107 @@
-from os import getenv
 from typing import Optional
 
-from dotenv import find_dotenv, load_dotenv
-from fastapi import APIRouter, HTTPException, status
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
 
-from src.infra.database import create_database_connection
-from src.models.post import Post
-
-load_dotenv(find_dotenv())
-DB_HOST: str = getenv("DB_HOST")
-DB_DATABASE: str = getenv("DB_DATABASE")
-DB_USER: str = getenv("DB_USER")
-DB_PASSWORD: str = getenv("DB_PASSWORD")
+from src.data import schemas
+from src.infra import models
+from src.infra.database import engine, get_db
 
 posts_routes = APIRouter()
-connection, cursor = create_database_connection(
-    host=DB_HOST,
-    database=DB_DATABASE,
-    user=DB_USER,
-    password=DB_PASSWORD,
-)
+models.Base.metadata.create_all(bind=engine)
 
 
 def check_if_post_exists(
-    id: Optional[int] = None, post: Optional[Post] = None
+    id: Optional[int] = None, post: Optional[schemas.Post] = None
 ) -> None:
+    # it has id but there are no data
     if id and not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Could not find post with id {id}.",
         )
-
+    # it does not have id and there are no data
     if not id and not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Could not find any post.",
         )
-
     return None
 
 
-@posts_routes.get("/api/v1/posts")
-async def get_all_posts() -> JSONResponse:
-    cursor.execute("SELECT * FROM posts")
-    posts = cursor.fetchall()
+@posts_routes.get(
+    "/api/v1/posts", status_code=status.HTTP_200_OK, response_model=dict
+)
+async def get_all_posts(db: Session = Depends(get_db)) -> dict:
+    posts: list = db.query(models.Posts).all()
     check_if_post_exists(post=posts)
-
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content={
-            "data": posts,
-            "links": {"self": "/posts", "root": "/"},
-        },
-    )
+    return {
+        "data": jsonable_encoder(posts),
+        "links": {"self": "/posts", "root": "/"},
+    }
 
 
-@posts_routes.get("/api/v1/posts/{id}")
-async def get_one_post(id: int) -> JSONResponse:
-    cursor.execute(f"SELECT * FROM posts WHERE id = {id}")
-    post = cursor.fetchone()
+@posts_routes.get(
+    "/api/v1/posts/{id}", status_code=status.HTTP_200_OK, response_model=dict
+)
+async def get_one_post(id: int, db: Session = Depends(get_db)) -> dict:
+    post = db.query(models.Posts).filter(models.Posts.id == id).first()
     check_if_post_exists(id=id, post=post)
-
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content={
-            "data": [post],
-            "links": {"self": f"/posts/{id}", "root": "/"},
-        },
-    )
+    return {
+        "data": jsonable_encoder([post]),
+        "links": {"self": f"/posts/{id}", "root": "/"},
+    }
 
 
-@posts_routes.post("/api/v1/posts")
-async def create_post(post: Post) -> JSONResponse:
-    cursor.execute(
-        """
-        INSERT INTO
-        posts (id, author, title, content)
-        VALUES (%s, %s, %s, %s)
-        RETURNING *
-        """,
-        (post.id, post.author, post.title, post.content),
-    )
-    post = cursor.fetchone()
-    connection.commit()
-
-    return JSONResponse(
-        status_code=status.HTTP_201_CREATED,
-        content={
-            "data": [post],
-            "links": {"self": "/posts", "root": "/"},
-        },
-    )
+@posts_routes.post(
+    "/api/v1/posts", status_code=status.HTTP_201_CREATED, response_model=dict
+)
+async def create_post(
+    post: schemas.Post, db: Session = Depends(get_db)
+) -> dict:
+    if db.query(models.Posts).filter(models.Posts.id == post.id).first():
+        return {"msg": "post already exists"}
+    post = models.Posts(**post.dict())
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    return {
+        "data": jsonable_encoder([post]),
+        "links": {"self": "/posts", "root": "/"},
+    }
 
 
-@posts_routes.delete("/api/v1/posts/{id}")
-async def delete_post(id: int) -> JSONResponse:
-    cursor.execute(f"DELETE FROM posts WHERE id = {id} RETURNING *")
-    post = cursor.fetchone()
+@posts_routes.delete(
+    "/api/v1/posts/{id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_model=None,
+)
+async def delete_post(id: int, db: Session = Depends(get_db)) -> None:
+    post_query = db.query(models.Posts).filter(models.Posts.id == id)
+    post = post_query.first()
     check_if_post_exists(id=id, post=post)
-    connection.commit()
+    post_query.delete(synchronize_session=False)
+    db.commit()
+    return None
 
-    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
 
-
-@posts_routes.put("/api/v1/posts/{id}")
-async def update_post(id: int, post: Post) -> JSONResponse:
-    cursor.execute(
-        """
-        UPDATE posts SET
-        author = %s,
-        title = %s,
-        content = %s
-        WHERE id = %s
-        RETURNING *
-        """,
-        (post.author, post.title, post.content, id),
-    )
-    post = cursor.fetchone()
-    check_if_post_exists(id=id, post=post)
-    connection.commit()
-
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content={
-            "data": [post],
-            "links": {"self": f"/posts/{id}", "root": "/"},
-        },
-    )
+@posts_routes.put(
+    "/api/v1/posts/{id}", status_code=status.HTTP_200_OK, response_model=dict
+)
+async def update_post(
+    id: int, post: schemas.Post, db: Session = Depends(get_db)
+) -> dict:
+    updated_post_query = db.query(models.Posts).filter(models.Posts.id == id)
+    updated_post = updated_post_query.first()
+    check_if_post_exists(id=id, post=updated_post)
+    updated_post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+    updated_post = updated_post_query.first()
+    return {
+        "data": jsonable_encoder([updated_post]),
+        "links": {"self": f"/posts/{id}", "root": "/"},
+    }
 
 
 """
