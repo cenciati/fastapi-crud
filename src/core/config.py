38a4a96b8387224fbc8fe0965ac7bc78c511de70
Gabriel <gbrcenciati@gmail.com>
2022-11-22 10:06:12 -0300

    add: url string

--- src/core/config.py
+++ src/core/config.py
@@ -1,8 +1,9 @@
-# from pydantic import BaseSettings
+from pydantic import BaseSettings
 
 
-# class Settings(BaseSettings):
-#     API_V1_STR: str = "/api/v1"
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    URL: str = f"http://localhost:8000{API_V1_STR}"
 
 
-# settings = Settings()
+settings = Settings()
