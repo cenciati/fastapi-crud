fe5b5db34e86ba977657bc02e3925629f5211642
Gabriel <gbrcenciati@gmail.com>
2022-11-22 10:09:12 -0300

    cut: jsonresponse fastapi object as function response

--- src/routes/root_routes.py
+++ src/routes/root_routes.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter, status
 
-root_routes = APIRouter()
+root_router = APIRouter()
 
 
-@root_routes.get(
-    "/api/v1", status_code=status.HTTP_200_OK, response_model=dict
-)
+@root_router.get("/", status_code=status.HTTP_200_OK, response_model=dict)
 async def root() -> dict:
     return {
         "data": ["Welcome to my personal blog!"],
