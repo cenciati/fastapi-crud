dfff6127f7a1cc083d2c7ce8f1cdf2b97256fa1d
Gabriel <gbrcenciati@gmail.com>
2022-11-22 10:17:23 -0300

    add: service layer

---
+++ src/services/posts_services.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from fastapi import HTTPException, status
+from src.data import schemas
+
+
+def check_if_posts_exist(posts: Optional[schemas.Post] = None) -> None:
+    if not posts:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Could not find any post.",
+        )
+    return None
+
+
+def check_if_post_exists(
+    id: Optional[int] = None, post: Optional[schemas.Post] = None
+) -> None:
+    if id and not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Could not find post with id {id}.",
+        )
+    return None
