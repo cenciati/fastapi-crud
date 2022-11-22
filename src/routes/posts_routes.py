f5392e24bb9c3c95493434d94efb2978d8401584
Gabriel <gbrcenciati@gmail.com>
2022-11-22 10:09:28 -0300

    cut: jsonresponse fastapi object as function response

--- src/routes/posts_routes.py
+++ src/routes/posts_routes.py
@@ -4,11 +4,12 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
+from src.core.config import settings
 from src.data import schemas
 from src.infra import models
 from src.infra.database import engine, get_db
 
-posts_routes = APIRouter()
+posts_router = APIRouter()
 models.Base.metadata.create_all(bind=engine)
 
 
@@ -30,32 +31,38 @@ def check_if_post_exists(
     return None
 
 
-@posts_routes.get(
-    "/api/v1/posts", status_code=status.HTTP_200_OK, response_model=dict
+@posts_router.get(
+    "/posts", status_code=status.HTTP_200_OK, response_model=dict
 )
 async def get_all_posts(db: Session = Depends(get_db)) -> dict:
     posts: list = db.query(models.Posts).all()
     check_if_post_exists(post=posts)
     return {
         "data": jsonable_encoder(posts),
-        "links": {"self": "/posts", "root": "/"},
+        "_links": {
+            "self": {"href": f"{settings.URL}/posts"},
+            "root": {"href": f"{settings.URL}/"},
+        },
     }
 
 
-@posts_routes.get(
-    "/api/v1/posts/{id}", status_code=status.HTTP_200_OK, response_model=dict
+@posts_router.get(
+    "/posts/{id}", status_code=status.HTTP_200_OK, response_model=dict
 )
 async def get_one_post(id: int, db: Session = Depends(get_db)) -> dict:
     post = db.query(models.Posts).filter(models.Posts.id == id).first()
     check_if_post_exists(id=id, post=post)
     return {
         "data": jsonable_encoder([post]),
-        "links": {"self": f"/posts/{id}", "root": "/"},
+        "_links": {
+            "self": {"href": f"{settings.URL}/posts/{id}"},
+            "root": {"href": f"{settings.URL}/"},
+        },
     }
 
 
-@posts_routes.post(
-    "/api/v1/posts", status_code=status.HTTP_201_CREATED, response_model=dict
+@posts_router.post(
+    "/posts", status_code=status.HTTP_201_CREATED, response_model=dict
 )
 async def create_post(
     post: schemas.Post, db: Session = Depends(get_db)
@@ -71,12 +78,15 @@ async def create_post(
     db.refresh(post)
     return {
         "data": jsonable_encoder([post]),
-        "links": {"self": "/posts", "root": "/"},
+        "_links": {
+            "self": {"href": f"{settings.URL}/posts"},
+            "root": {"href": f"{settings.URL}/"},
+        },
     }
 
 
-@posts_routes.delete(
-    "/api/v1/posts/{id}",
+@posts_router.delete(
+    "/posts/{id}",
     status_code=status.HTTP_204_NO_CONTENT,
     response_model=None,
 )
@@ -89,8 +99,8 @@ async def delete_post(id: int, db: Session = Depends(get_db)) -> None:
     return None
 
 
-@posts_routes.put(
-    "/api/v1/posts/{id}", status_code=status.HTTP_200_OK, response_model=dict
+@posts_router.put(
+    "/posts/{id}", status_code=status.HTTP_200_OK, response_model=dict
 )
 async def update_post(
     id: int, post: schemas.Post, db: Session = Depends(get_db)
@@ -103,7 +113,10 @@ async def update_post(
     updated_post = updated_post_query.first()
     return {
         "data": jsonable_encoder([updated_post]),
-        "links": {"self": f"/posts/{id}", "root": "/"},
+        "_links": {
+            "self": {"href": f"{settings.URL}/posts/{id}"},
+            "root": {"href": f"{settings.URL}/"},
+        },
     }
 
 
