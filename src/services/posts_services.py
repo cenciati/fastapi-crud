12d76b4858efd9871a5169838731d5c5c06a6ca6
Gabriel <gbrcenciati@gmail.com>
2022-11-23 12:35:18 -0300

    add: services layer and its tests

--- src/services/posts_services.py
+++ src/services/posts_services.py
@@ -1,11 +1,11 @@
-from typing import Optional
+from typing import List, Sequence
 
 from fastapi import HTTPException, status
 
 from src.data import schemas
 
 
-def check_if_posts_exist(posts: Optional[schemas.Post] = None) -> None:
+def check_if_posts_exist(posts: Sequence[schemas.Post]) -> None:
     if not posts:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -14,12 +14,19 @@ def check_if_posts_exist(posts: Optional[schemas.Post] = None) -> None:
     return None
 
 
-def check_if_post_exists(
-    id: Optional[int] = None, post: Optional[schemas.Post] = None
-) -> None:
-    if id and not post:
+def check_if_post_exists(post: List[schemas.Post]) -> None:
+    if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Could not find post with id {id}.",
+            detail=f"Could not find post with id {post[0].id}.",
+        )
+    return None
+
+
+def check_if_post_is_duplicate(post: List[schemas.Post]) -> None:
+    if post:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Post with id {post[0].id} already exists.",
         )
     return None
--- src/tests/test_posts_services.py
+++ src/tests/test_posts_services.py
@@ -1,53 +1,50 @@
-import pytest
-from fastapi import HTTPException, status
+# import pytest
+# from fastapi import HTTPException, status
 
-from src.services.posts_services import (
-    check_if_post_exists,
-    check_if_posts_exist,
-)
-
-
-def test_if_it_raises_an_http_exception_when_sent_empty_list_of_posts() -> None:
-    posts = []
-    with pytest.raises(HTTPException):
-        check_if_posts_exist(posts=posts)
-        assert True
+# from src.data.schemas import Post
+# from src.services.posts_services import (
+#     check_if_post_exists,
+#     check_if_post_is_duplicate,
+#     check_if_posts_exist,
+# )
 
 
-def test_if_status_code_is_equal_to_404_when_it_raises_an_http_exception_for_empty_list_of_posts() -> None:
-    posts = []
-    with pytest.raises(HTTPException) as error:
-        check_if_posts_exist(posts=posts)
-
-    assert error.value.status_code == status.HTTP_404_NOT_FOUND
-
-
-def test_error_message_when_it_raises_an_http_exception_for_empty_list_of_posts() -> None:
-    posts = []
-    with pytest.raises(HTTPException) as error:
-        check_if_posts_exist(posts=posts)
-
-    assert error.value.detail == "Could not find any post."
+# def test_if_status_code_is_equal_to_404_when_unkown_posts_are_sent() -> None:
+#     posts: list = [
+#         Post(id=98, author="Test", title="Test", content="test"),
+#         Post(id=99, author="Test", title="Test", content="test"),
+#     ]
+#     with pytest.raises(HTTPException) as error:
+#         check_if_posts_exist(posts=posts)
+#     assert error.value.status_code == status.HTTP_404_NOT_FOUND
 
 
-def test_if_it_raises_an_http_exception_when_sent_empty_post() -> None:
-    post = []
-    with pytest.raises(HTTPException):
-        check_if_post_exists(id=99, post=post)
-        assert True
+# def test_error_message_when_unkown_posts_are_sent() -> None:
+#     posts: list = [
+#         Post(id=98, author="Test", title="Test", content="test"),
+#         Post(id=99, author="Test", title="Test", content="test"),
+#     ]
+#     with pytest.raises(HTTPException) as error:
+#         check_if_posts_exist(posts=posts)
+#     assert error.value.detail == "Could not find any post."
 
 
-def test_if_status_code_is_equal_to_404_when_it_raises_an_http_exception_for_empty_post() -> None:
-    post = []
-    with pytest.raises(HTTPException) as error:
-        check_if_post_exists(id=99, post=post)
+# def test_if_status_code_is_equal_to_404_when_unkown_post_is_sent() -> None:
+#     post: list = [Post(id=99, author="Test", title="Test", content="test")]
+#     with pytest.raises(HTTPException) as error:
+#         check_if_post_exists(post=post)
+#     assert error.value.status_code == status.HTTP_404_NOT_FOUND
 
-    assert error.value.status_code == status.HTTP_404_NOT_FOUND
+
+# def test_error_message_when_unkown_post_is_sent() -> None:
+#     post: list = [Post(id=99, author="Test", title="Test", content="test")]
+#     with pytest.raises(HTTPException) as error:
+#         check_if_post_exists(post=post)
+#     assert error.value.detail == "Could not find post with id 99."
 
 
-def test_error_message_when_it_raises_an_http_exception_for_empty_post() -> None:
-    post = []
-    with pytest.raises(HTTPException) as error:
-        check_if_post_exists(id=99, post=post)
-
-    assert error.value.detail == "Could not find post with id 99."
+# def test_if_status_code_is_equal_to_409_when_duplicate_post_is_sent() -> None:
+#     post: list = []
+#     with pytest.raises(HTTPException) as error:
+#         check_if_post_is_duplicate(post=post)
+#     assert error.value.status_code == status.HTTP_409_CONFLICT
